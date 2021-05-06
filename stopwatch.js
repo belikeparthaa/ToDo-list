618b069bd8414fc7cb014b469d437e92feaeff8a
belikeparthaa <37862228+belikeparthaa@users.noreply.github.com>
2021-05-06 23:14:44 +0530

    Add files via upload

---
+++ stopwatch.js
@@ -0,0 +1,87 @@
+let seconds=0;
+let minutes=0;
+let hours=0;
+
+let displaySeconds=0;
+let displayMinute=0;
+let displayHours=0;
+
+let interval=null;
+
+let status="stopped";
+
+function stopWatch()
+{
+    seconds++;
+
+    if(seconds/60 === 1)
+    {
+        seconds=0;
+        minutes++;
+        if(minutes/60 === 1)
+        {
+            minutes=0;
+            hours++;
+        }
+    }
+
+    if(seconds<10)
+    {
+        displaySeconds="0"+ seconds.toString();
+    }
+    else
+    {
+        displaySeconds=seconds;
+    }
+
+    if(minutes<10)
+    {
+        displayMinute="0"+ minutes.toString();
+    }
+    else
+    {
+        displayMinute=minutes;
+    }
+
+    if(hours<10)
+    {
+        displayHours="0"+ hours.toString();
+    }
+    else
+    {
+      displayHours=hours;
+    }
+
+
+    document.getElementById("display").innerHTML= displayHours+":" + displayMinute + ":" + displaySeconds;
+}
+
+
+
+function startStop()
+{
+    if(status === "Stopped")
+    {
+        interval= window.setInterval(stopWatch, 1000);
+        document.getElementById("startStop").innerHTML="Stop";
+        status="started";
+    }
+
+    else
+    {
+        window.clearInterval(interval);
+        document.getElementById("startStop").innerHTML="Start";
+        status="Stopped"
+    }
+}
+
+
+function reset()
+{
+    window.clearInterval(interval);
+    seconds=0;
+    minutes=0;
+    hours=0;
+    document.getElementById("display").innerHTML="00:00:00";
+    document.getElementById("startStop").innerHTML="Start";
+}
